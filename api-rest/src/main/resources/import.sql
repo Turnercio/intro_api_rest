3871510c6d9bce04f80cc6c15e848df3c5101509
Rubén Fresneda <cem20814@gmail.com>
2024-06-25 20:06:14 +0200

    final stage

---
+++ api-rest/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Jose','Bonaparte','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Sergio','Peris','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Ruben','Fresneda','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Maria','Lacena','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Ruben','Lopez','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Raul','Lopez','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Borja','Ruiz','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Jose','Perez','assd@gmail.com','963759593','2024-06-20')
+INSERT INTO cliente(nombre, apellido, email, telefono, fecha_creacion) VALUES ('Angela','Lopez','assd@gmail.com','963759593','2024-06-20')
+
